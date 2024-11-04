c6fae6ba848801ff310c52a939c6cddf52edfb0d
Nirmal1901 <74503593+Nirmal1901@users.noreply.github.com>
2024-11-04 23:36:00 +0530

    Create first.py

---
+++ folder/first.py
@@ -0,0 +1,17 @@
+
+class Car():
+    def __init__(self, brand=None, model=None):
+        self.brand = brand if brand else "Unknown"
+        self.model = model if model else "Unknown"
+
+    def set_brand(self, brand):
+        self.brand = brand
+
+    def get_brand(self):
+        return self.brand
+
+    def set_model(self, model):
+        self.model = model
+
+    def get_model(self):
+        return self.model
