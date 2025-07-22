ccef1719979e6b6ad492bc44db3c66d529c5665f
Nirmal1901 <74503593+Nirmal1901@users.noreply.github.com>
2025-07-22 09:36:29 +0530

    Update system.java

--- system.java
+++ system.java
@@ -1,1 +1,21 @@
-system.out.println("Hello world");
+package com.bad.example; 
+
+
+public class VeryWrongSpringBootApp {
+
+    public static void main(String[] args) {
+        System.out.println("I think this is Spring Boot?"); // Not starting Spring
+    }
+
+    @GetMapping("/danger") 
+    public String dangerZone() {
+        return 123; // Returning int instead of String
+    }
+
+    @PostMapping("/crash") // No request mapping, wrong annotation without dependencies
+    public void crashServer() {
+        throw new NullPointerException(); 
+    }
+
+ 
+}
