e268f5efe0ca93563f868c86bd83dc035a176122
Nirmal1901 <74503593+Nirmal1901@users.noreply.github.com>
2025-09-30 10:14:18 +0530

    Update correct.java

--- correct.java
+++ correct.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(DemoApplication.class, args);
     }
 
